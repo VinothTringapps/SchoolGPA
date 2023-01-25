40bf3186631b461d8524e681aae8e896215312fd
Tringapps-User20 <vinothkumarp.pukalenthi@tringapps.com>
2023-01-25 22:47:27 +0530

    upload

---
+++ src/main/java/org/example/School.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.Scanner;
+
+public class School {
+    String studentName,gradeLevel;
+    double gpa;
+    School(String studentName,String gradeLevel,double gpa){
+        this.studentName=studentName;
+        this.gradeLevel=gradeLevel;
+        this.gpa=gpa;
+    }
+    public void getGpa(double gpa){
+        this.gpa = gpa;
+    }
+    public void  details(){
+        System.out.println(this.studentName+" has a "+this.gpa+" GPA");
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\nEnter the Name :");
+        String name=sc.nextLine();
+        System.out.print("\nEnter the GradeLevel :");
+        String gradeLevel=sc.nextLine();
+        System.out.print("\nEnter the GPA :");
+        double gpa=sc.nextDouble();
+
+        School s=new School(name,gradeLevel,gpa);
+        System.out.println("Updated GPA :");
+        gpa=sc.nextDouble();
+        s.getGpa(gpa);
+        s.details();
+
+    }
+}
